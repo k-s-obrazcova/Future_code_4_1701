12dee5880a39b7dfff919ac76922b2ca3d182edd
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-03 12:33:14 +0300

    Первая версия игры

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 
 number_to_guess = random.randint(1, 1000)
 guess = None
+attempts = 0
 
 print("""
 ______________________________________________
@@ -12,10 +13,10 @@ ______________________________________________
 """)
 while guess != number_to_guess:
     guess = int(input("Введи свое число: "))
-
+    attempts += 1
     if guess < number_to_guess:
         print("Ты был близок, но пока мало")
     elif guess > number_to_guess:
         print("Ты был близок, но слишком много")
 
-print(f"Поздравляю, вы угадали число {number_to_guess}")
+print(f"Поздравляю, вы угадали число {number_to_guess}, угадано за {attempts} попыток")
