46885528e800adadfd09d4b20d56e6e73e2194b8
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-10 12:49:31 +0300

    Notepad

--- notepad.py
+++ notepad.py
@@ -4,6 +4,17 @@ def add_event(filename, event):
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(str(datetime.datetime.now()) + " " + event + "\n")
 
+def modify_event(filename, index, new_event):
+    with open(filename, 'r+', encoding='utf-8') as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate()
+        for i, line in enumerate(lines):
+            if i == index - 1:
+                file.write(str(datetime.datetime.now()) + ' ' + new_event + '\n')
+            else:
+                file.write(line)
+
 def menu():
     filename = 'notepad.txt'
     while True:
@@ -16,7 +27,9 @@ def menu():
         if choice == '1':
             event = input("Введите событие: ")
             add_event(filename, event)
-
-
+        elif choice == '2':
+            index = int(input("Введите номер строки для изменения: "))
+            new_event = input("Введите событие: ")
+            modify_event(filename, index, new_event)
 
 menu()
