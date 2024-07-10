e6ad9725e175a0772c3141c554376e03d2610bf4
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-10 13:48:50 +0300

    Notepad

---
+++ school.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+def create_user(name, role):
+    return {"name": name, "role": role, "grades": []}
+def load_data(filename='school.json'):
+    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    else:
+        return []
+
+
+def menu():
+    users = load_data()
+    while True:
+        print("1. Создать пользователя")
+        print("2. Добавить оценку студенту")
+        print("3. Посмотреть данные студента")
+        print("4. Посмотреть данные преподавателя")
+        print("5. Выход")
+        choice = input("Выберите действие: ")
+        if choice == '1':
+            name = input("Введите имя пользователя")
+            role = input("Введите роль (student / teacher)")
+            users.append(create_user(name, role))
+
+
