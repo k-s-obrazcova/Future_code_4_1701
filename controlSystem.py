b8bf9d16d687982e48f7c7fe5add6deff0b0fb1f
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-11 13:52:56 +0300

    new

--- controlSystem.py
+++ controlSystem.py
@@ -1,11 +1,35 @@
 import json
 
+
 def load_data():
     try:
         with open('users.json', 'r') as file:
             return json.load(file)
     except FileNotFoundError:
         return {"users": []}
+
+
+def save_data(users):
+    with open('users.json', 'w') as file:
+        json.dump(users, file)
+
+
+def register(username, password, role, users):
+    for user in users["users"]:
+        if user["username"] == username:
+            print("Пользователь с таким ником уже существует!")
+            return
+
+    new_user = {
+        "username": username,
+        "password": password,
+        "role": role
+    }
+
+    users["users"].append(new_user)
+    save_data(users)
+    print("Пользователь успешно создан")
+
 
 def menu():
     data = load_data()
@@ -18,3 +42,10 @@ def menu():
         print("5. Выход; ")
 
         choice = input("Выберите действие: ")
+        if choice == "1":
+            username = input("Введите никнейм пользователя: ")
+            password = input("Введите пароль пользователя:")
+            role = input("Введите роль(1.Admin; 2.User)")
+            register(username=username, password=password, role=role, users=data)
+
+menu()
