a07685c0eb760c175c0e41cdf2089de794908de2
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-11 12:19:51 +0300

    new

--- school.py
+++ school.py
@@ -10,19 +10,24 @@ def load_data(filename='school.json'):
     else:
         return []
 
+def save_data(users, filename='school.json'):
+    with open(filename, 'w') as file:
+        json.dump(users, file)
 
 def menu():
     users = load_data()
     while True:
-        print("1. Создать пользователя")
-        print("2. Добавить оценку студенту")
-        print("3. Посмотреть данные студента")
-        print("4. Посмотреть данные преподавателя")
+        print("1. Создать пользователя: ")
+        print("2. Добавить оценку студенту: ")
+        print("3. Посмотреть данные студента: ")
+        print("4. Посмотреть данные преподавателя: ")
         print("5. Выход")
         choice = input("Выберите действие: ")
         if choice == '1':
             name = input("Введите имя пользователя")
             role = input("Введите роль (student / teacher)")
             users.append(create_user(name, role))
+            save_data(users)
 
 
+menu()
