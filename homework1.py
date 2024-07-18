b78f40dd0c8e532125840e57a3cab80c9dbf97e3
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-18 13:38:49 +0300

    Homework

--- homework1.py
+++ homework1.py
@@ -32,3 +32,57 @@
 #
 #
 # print(current_time_in_shanghai())
+
+
+# 3.	На вход подается дата рождения пользователя. Необходимо вычислить его возраст в днях.
+
+# from datetime import datetime
+#
+#
+# def days_birthday(birthday):
+#     birthday = datetime.strptime(birthday, "%Y-%m-%d")
+#     now = datetime.now()
+#     return (now - birthday).days
+#
+#
+# birthday = input("Введите свою дату рождения: ")
+# print(days_birthday(birthday))
+
+# 4 На вход подается дата и время начала события и его продолжительность в часах.
+# Необходимо определить дату и время окончания события.
+
+
+# from datetime import datetime, timedelta
+#
+#
+# def event_end(start_date_time, duration_hours):
+#     start_date_time = datetime.strptime(start_date_time, "%Y-%m-%d %H:%M:%S")
+#     end_date_time = start_date_time + timedelta(hours=duration_hours)
+#     return end_date_time
+#
+#
+# print(event_end("2024-07-18 13:19:00", 1))
+
+
+# 5.	Дано время отправления самолета из аэропорта в Москве и время полета до Нью-Йорка.
+# Необходимо определить местное время прибытия самолета в Нью-Йорк.
+# Учтите, что время полета указано в часах,
+# а разница во времени между Москвой и Нью-Йорком составляет -7 часов.
+
+# from datetime import datetime, timedelta
+# import pytz
+#
+#
+# def arrival(departure_time, flight_time):
+#     moscow_timezone = pytz.timezone('Europe/Moscow')
+#     ny_timezone = pytz.timezone('America/New_York')
+#     departure_time = datetime.strptime(departure_time, "%Y-%m-%d %H:%M:%S")
+#     departure_time = moscow_timezone.localize(departure_time)
+#     arrival_time_ny = departure_time + timedelta(hours=int(flight_time))
+#     arrival_time_ny = arrival_time_ny.astimezone(ny_timezone)
+#     return arrival_time_ny
+#
+#
+# departure = input("Введите время и дату отправки в формате YYYY-MM-DD HH:MM:SS : ")
+# flight = input("Введите количество часов в полёте: ")
+# print(arrival(departure_time=departure, flight_time=flight))
