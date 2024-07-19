28e086f2ff0edae80424a9a72a51f4ba40398168
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-19 13:42:06 +0300

    SystemStatistic

--- exercise.py
+++ exercise.py
@@ -1,8 +1,35 @@
-import glob
-import os.path
+# 1
+# import glob
+# import os.path
+#
+# files = glob.glob('./*.py')
+# for file in files:
+#     print(file)
+#     file_size = os.path.getsize(file)
+#     print(f"Размер файла {file} - {file_size} байт")
 
-files = glob.glob('./*.py')
-for file in files:
-    print(file)
-    file_size = os.path.getsize(file)
-    print(f"Размер файла {file} - {file_size} байт")
+# 2
+import csv
+import glob
+import datetime
+import statistics
+import timeit
+
+
+def work_files():
+    data = {}
+    for filename in glob.glob('*.csv'):
+        try:
+            date = datetime.datetime.strptime(filename, '%Y-%m-%d.csv').date()
+        except ValueError:
+            print(f"Файл {filename} был пропущен так как не соответствует формату")
+            continue
+        with open(filename, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            times, temps, humids = zip(
+                *[(datetime.datetime.strptime(row[0], '%H:%M:S').time(),
+                   float(row[1]),
+                   float(row[2])) for row in reader])
+            data[date] = {'times': times, 'temps': temps, 'humids': humids}
+    return data
