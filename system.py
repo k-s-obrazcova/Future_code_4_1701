b1969cd7b16001476585789fabc1c52c1b640d80
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-19 12:20:22 +0300

    SystemStatistic

--- system.py
+++ system.py
@@ -3,12 +3,19 @@ import pandas
 import statistics
 import timeit
 
+
 def calculate_time_and_weight(directory, column):
     start_time = timeit.default_timer()
     file = glob.glob(directory + "/zoo.csv")
     if file:
-        data = pandas.read_csv(file[0])
+        data = pandas.read_csv(file[0], encoding='windows-1251')
         if column in data.columns:
-
+            data[column] = data[column].str.replace(',', '.').astype(float)
+            mean = statistics.mean(data[column])
         else:
-            return f"Столбик с названием {column} не найден"
+            return f"РЎС‚РѕР»Р±РёРє {column} РЅРµ РЅР°Р№РґРµРЅ"
+    else:
+        return f"Р¤Р°Р№Р» zoo.csv РЅРµ РЅР°Р№РґРµРЅ РІ РґРёСЂРµРєС‚РѕСЂРёРё {directory}"
+    end_time = timeit.default_timer()
+    work_time = end_time - start_time
+    return mean, work_time
