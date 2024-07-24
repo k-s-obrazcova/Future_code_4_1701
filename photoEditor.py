e47b064f37ca79638643be2dc2ab6edb5280e49a
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-24 13:21:14 +0300

    New

---
+++ photoEditor.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import filedialog, colorchooser
+from PIL import Image, ImageTk, ImageFilter, ImageOps
+
+
+class ImageEditor:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1000x600")
+        self.original_image = None
+        self.modified_image = None
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.open_button = tk.Button(self.root, text="Открыть изображение", command=self.open_image)
+        self.open_button.pack()
+
+        self.image_label = tk.Label(self.root)
+        self.image_label.pack()
+
+    def open_image(self):
+        filename = filedialog.askopenfilename(
+            filetypes=(("PNG files", "*.png"), ("JPEG/JPG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+        if filename:
+            self.original_image = Image.open(filename)
+            self.modified_image = self.original_image.copy()
+            self.display_image(self.modified_image)
+
+    def display_image(self, image):
+        width, height = image.size
+        if width > height:
+            if width > 400:
+                ratio = 400 / width
+            else:
+                ratio = 1
+        else:
+            if height > 400:
+                ratio = 400 / height
+
+            else:
+                ratio = 1
+        image = image.resize(
+            (int(width * ratio), (int(height * ratio)))
+        )
+        photo = ImageTk.PhotoImage(image)
+        self.image_label.config(image=photo)
+        self.image_label.image = photo
+
+
+root = tk.Tk()
+root.title("Image Editor")
+editor = ImageEditor(root)
+root.mainloop()
