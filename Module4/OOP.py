161170b3cea6e33eb8885b7ed633a9e270e0d93a
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-08-19 13:13:44 +0300

    19.08

--- Module4/OOP.py
+++ Module4/OOP.py
@@ -17,15 +17,37 @@
 # функцию добавлено больше 2-х цифр, то они также должны быть
 # просуммированы и возвращены в качестве результата.
 
-def digit_sum(*args):
-    result = 0
-    for num in args:
-        if isinstance(num, (int, float)):
-            result += num
-    return result
+# def digit_sum(*args):
+#     result = 0
+#     for num in args:
+#         if isinstance(num, (int, float)):
+#             result += num
+#     return result
+#
+#
+# print(digit_sum(1, 2))
+# print(digit_sum(3, 4, 5))
+# print(digit_sum(1.5, 2.5, 3.5))
+# print(digit_sum(1, "2", 3.5))
 
 
-print(digit_sum(1, 2))
-print(digit_sum(3, 4, 5))
-print(digit_sum(1.5, 2.5, 3.5))
-print(digit_sum(1, "2", 3.5))
+# ООП
+class Person:
+    def __init__(self, name, age, birthday, number, seria):
+        self.name = name
+        self.age = age
+        self.birthday = birthday
+        self.number_passport = number
+        self.seria_passport = seria
+
+    def speak(self):
+        return (f"Мое имя {self.name}, день рождения {self.birthday}."
+                f" Мне {self.age}. Паспортные данные {self.seria_passport} {self.number_passport}")
+
+
+person_first = Person("Ксения", 26, "30.05.1997", 474145, 8456)
+person_second = Person("Олег", 21, "15.09.2002", 789781, 1256)
+print(person_first.speak())
+print(person_second.speak())
+print(person_first.age)
+print(person_first.name)
