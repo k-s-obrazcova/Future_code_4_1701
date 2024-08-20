129e0df4bed9d64b373982741f5e7e959d61ee3b
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-08-20 15:50:10 +0300

    20.08

--- Module4/2.py
+++ Module4/2.py
@@ -21,3 +21,34 @@ class Manager(Employee):
 
     def list_subordinates(self):
         return [str(subordinate) for subordinate in self.subordinates]
+
+
+class Department:
+    def __init__(self):
+        self.employees = []
+
+    def add_employee(self, employee):
+        self.employees.append(employee)
+
+    def list_employees(self):
+        return [str(employee) for employee in self.employees]
+
+employee_first = Employee("Иван Иванов", "Инженер", 50000)
+employee_second = Employee("Петр Петров", "Аналитик", 60000)
+manager_first = Manager("Анна Смирнова", "Менеджер по закупкам", 80000)
+
+department = Department()
+department.add_employee(employee_first)
+department.add_employee(employee_second)
+department.add_employee(manager_first)
+
+manager_first.add_subordinate(employee_first)
+manager_first.add_subordinate(employee_second)
+
+print("Все сотрудники отдела: ")
+for emp in department.list_employees():
+    print(emp)
+
+print("Подчиненные менеджера: ")
+for emp_sub in manager_first.list_subordinates():
+    print(emp_sub)
