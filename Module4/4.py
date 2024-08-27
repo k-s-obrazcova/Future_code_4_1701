4620ef481fcb0f92ff1dc2f71c4ae187148df156
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-08-27 12:41:15 +0300

    27.08

--- Module4/4.py
+++ Module4/4.py
@@ -31,7 +31,7 @@ class Car:
         self.body = body
         self.weight = self.calculate_weight()
         self.max_range = self.calculate_max_range()
-        self.braking_distance
+        self.braking_distance = self.calculate_braking_distance()
 
     def calculate_weight(self):
         return self.engine.weight + self.fuel_tank.weight + self.brakes.weight + self.body.weight
@@ -40,3 +40,40 @@ class Car:
         return self.fuel_tank.volume * self.engine.max_speed / self.engine.fuel_consumption
 
     def calculate_braking_distance(self):
+        return self.weight * self.brakes.effect
+
+
+class Collector:
+    def __init__(self):
+        self.engine = None
+        self.fuel_tank = None
+        self.brakes = None
+        self.body = None
+
+    def set_engine(self, weight, max_speed, fuel_consumption):
+        self.engine = Engine(weight, max_speed, fuel_consumption)
+
+    def set_fuel_tank(self, weight, volume):
+        self.fuel_tank = FuelTank(weight, volume)
+
+    def set_brakes(self, weight, effect):
+        self.brakes = Brakes(weight, effect)
+
+    def set_body(self, weight):
+        self.body = Body(weight)
+
+    def build_car(self, name):
+        return Car(name, self.engine, self.fuel_tank, self.brakes, self.body)
+
+
+collector = Collector()
+collector.set_engine(200, 40, 10)
+collector.set_fuel_tank(100, 50)
+collector.set_brakes(50, 0.5)
+collector.set_body(500)
+car = collector.build_car("MyCar")
+
+print(car.name)
+print(car.weight)
+print(car.max_range)
+print(car.braking_distance)
